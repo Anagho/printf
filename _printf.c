832e172888b5056834ca7d878da4b40fced55103
ANAGHO OKIRORO OBRUTHE <66509601+Anagho@users.noreply.github.com>
2023-04-27 08:08:25 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,45 +1,32 @@
-#include <stdio.h>
-#include <stdarg.h>
+/**
+ * _printf - function that replicates what printf does
+ * @format: a character string
+ *
+ * Return:  the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	print_type argument[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"%", _print_percent},
+		{"d", _print_int},
+		{"i", _print_int},
+		{"r", _print_reverse},
+		{"R", _print_rot13},
+		{"b", _print_binary},
+		{"u", _print_unsigned},
+		{"o", _print_octal},
+		{"x", _print_hex_l},
+		{"X", _print_hex_u},
+		{NULL, NULL}
+	};
+	va_list ap;
+	int count = 0;
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    
-    int num_chars_printed = 0;
-    char c;
-    while ((c = *format++) != '\0') {
-        if (c == '%') {
-            c = *format++;
-            if (c == 'c') {
-                // Print a character
-                char ch = (char) va_arg(args, int);
-                putchar(ch);
-                num_chars_printed++;
-            } else if (c == 's') {
-                // Print a string
-                char *str = va_arg(args, char *);
-                while (*str != '\0') {
-                    putchar(*str++);
-                    num_chars_printed++;
-                }
-            } else if (c == '%') {
-                // Print a percent symbol
-                putchar('%');
-                num_chars_printed++;
-            } else {
-                // Unknown conversion specifier
-                putchar('%');
-                putchar(c);
-                num_chars_printed += 2;
-            }
-        } else {
-            // Just print the character
-            putchar(c);
-            num_chars_printed++;
-        }
-    }
-    
-    va_end(args);
-    
-    return num_chars_printed;
+
+	va_start(ap, format);
+	count = get_print(format, argument, ap);
+	va_end(ap);
+	return (count);
 }
